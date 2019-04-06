471a339ba9efdd547128acf1ab37c5d4e0ef849c
Petre Diana <petre.diana98@yahoo.ro>
2019-04-06 17:49:30 +0300

    fisier + stergereLD

--- miscellaneous/EXERCITII/Fisier_LD.c
+++ miscellaneous/EXERCITII/Fisier_LD.c
@@ -138,6 +138,72 @@ void dezalocare_LD(Nod_LD **prim) {
 	}
 }
 
+void extragere_LD(Nod_LD **prim, Nod_LD **ultim, char *extensie, Fisier *extras) {
+	
+	if ((*prim) == NULL && (*ultim) == NULL)
+		return;
+	else {
+		//cazul 1: extrag primul element al listei
+		if (strcmp((*prim)->info.extensie_fisier, extensie) == 0) {
+			(*extras).denumire_fisier = copiaza_variabila_char((*prim)->info.denumire_fisier);
+			(*extras).extensie_fisier = copiaza_variabila_char((*prim)->info.extensie_fisier);
+			(*extras).path_fisier = copiaza_variabila_char((*prim)->info.path_fisier);
+			(*extras).dim_bytes = (*prim)->info.dim_bytes;
+
+			Nod_LD *temp = *prim;
+			(*prim) = (*prim)->next;
+			(*prim)->prev = NULL;
+
+			free(temp->info.denumire_fisier);
+			free(temp->info.extensie_fisier);
+			free(temp->info.path_fisier);
+			free(temp);
+		}
+		else {
+			//cazul 2: extrag ultimul nod al listei
+			if (strcmp((*ultim)->info.extensie_fisier, extensie) == 0) {
+				(*extras).denumire_fisier = copiaza_variabila_char((*prim)->info.denumire_fisier);
+				(*extras).extensie_fisier = copiaza_variabila_char((*prim)->info.extensie_fisier);
+				(*extras).path_fisier = copiaza_variabila_char((*prim)->info.path_fisier);
+				(*extras).dim_bytes = (*prim)->info.dim_bytes;
+
+				Nod_LD *temp = (*ultim);
+				(*ultim) = (*ultim)->prev;
+				(*ultim)->next = NULL;
+				
+				free(temp->info.denumire_fisier);
+				free(temp->info.extensie_fisier);
+				free(temp->info.path_fisier);
+				free(temp);
+			}
+			else {
+				//cazul 3: extrag un nod de la mijloc
+				Nod_LD *temp = (*prim);
+
+				while (temp) {
+					if (strcmp(temp->info.extensie_fisier, extensie) == 0) {
+						(*extras).denumire_fisier = copiaza_variabila_char((*prim)->info.denumire_fisier);
+						(*extras).extensie_fisier = copiaza_variabila_char((*prim)->info.extensie_fisier);
+						(*extras).path_fisier = copiaza_variabila_char((*prim)->info.path_fisier);
+						(*extras).dim_bytes = (*prim)->info.dim_bytes;
+						break;
+					}
+					temp = temp->next;
+				}
+				
+				Nod_LD *temp2 = temp;
+				temp->prev->next = temp->next;
+				temp->next->prev = temp->prev;
+
+				free(temp2->info.denumire_fisier);
+				free(temp2->info.extensie_fisier);
+				free(temp2->info.path_fisier);
+				free(temp2);
+			}
+		}
+	}
+}
+
 int main()
 {
 	Fisier f1 = creare_fisier("Studenti", "D:\\folder1\\Acasa", ".txt", 30);
@@ -145,6 +211,7 @@ int main()
 	Fisier f3 = creare_fisier("NoteExamen", "D:\\folder3\\note", ".word", 70);
 	Fisier f4 = creare_fisier("Partituri", "D:\\folder4\\Muzica", ".txt", 20);
 	Fisier f5 = creare_fisier("DeseneAnimate", "C:\\folder5\\Filme", ".wav", 30);
+	Fisier f6 = creare_fisier("aaaaa", "random", ".wav", 30);
 
 	Nod_LD *cap = NULL;
 	Nod_LD *coada = NULL;
@@ -154,6 +221,7 @@ int main()
 	inserare_LD(&cap, &coada, f3);
 	inserare_LD(&cap, &coada, f4);
 	inserare_LD(&cap, &coada, f5);
+	inserare_LD(&cap, &coada, f6);
 
 	printf("\nTRAVERSARE LISTA DUBLA DUPA INSERARE");
 	printf("\n\n\tTraversare Normala");
@@ -175,6 +243,21 @@ int main()
 	modifica_LD(cap, f5.extensie_fisier, "D:\\MODIFICAT-haha\\AlteFilme");
 
 	printf("\nTRAVERSEZ LISTA CA SA VAD MODIFICAREA");
+	traversare_normala_LD(cap);
+
+
+	printf("\n\n-------------------");
+	Fisier f;
+	printf("\nSTERG PRIMUL NOD AL LISTEI");
+	extragere_LD(&cap, &coada, f1.extensie_fisier, &f);
+	traversare_normala_LD(cap);
+
+	printf("\n\nTERG ULTIMUL NOD AL LISTEI");
+	extragere_LD(&cap, &coada, f6.extensie_fisier, &f);
+	traversare_normala_LD(cap);
+
+	printf("\n\nSTERG UN NOD AL LISTEI DE LA MIJLOC");
+	extragere_LD(&cap, &coada, f3.extensie_fisier, &f);
 	traversare_normala_LD(cap);
 
 	dezalocare_LD(&cap);
